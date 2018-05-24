adfb5c3337795fa66a0bd9d7275f4c5b00ee7cf4
Parikshit Hooda <phooda804@live.com>
2018-05-24 17:04:29 +0530

    adapt to heroku prod

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-
 /*
  * routes/index.js
  * 
@@ -13,38 +12,38 @@ var Food = require("../models/model.js"); //our db model
 
 // home page route to show all suggested foods
 exports.index = function(req, res) {
-	
-	console.log("main page requested");
 
-	// we want to show all the foods that are already in the database
+    console.log("main page requested");
 
-	// query for all foods
-	// .find will accept 4 arguments
-	//  see http://mongoosejs.com/docs/api.html#model_Model.find
-	// 1) an object for filtering {} (empty here)
-	// 2) a string of properties to return, 'name slug upvotes photo recommendedB' will return only those attributes of the model
-	// 3) an object for sorting {sort:'-upvotes'}, the most upvoted will come first
-	// 4) callback function with (err, data)
-	//    err will include any error that occurred
-	//	  data is our resulting array of food objects returned from the database
-	Food.find({}, 'name slug upvotes photo recommendedBy',{sort:'-upvotes'}, function(err, data){
+    // we want to show all the foods that are already in the database
 
-		if (err) {
-			console.log(err);
-			res.send("Unable to query database for food").status(500);
-		};
+    // query for all foods
+    // .find will accept 4 arguments
+    //  see http://mongoosejs.com/docs/api.html#model_Model.find
+    // 1) an object for filtering {} (empty here)
+    // 2) a string of properties to return, 'name slug upvotes photo recommendedB' will return only those attributes of the model
+    // 3) an object for sorting {sort:'-upvotes'}, the most upvoted will come first
+    // 4) callback function with (err, data)
+    //    err will include any error that occurred
+    //	  data is our resulting array of food objects returned from the database
+    Food.find({}, 'name slug upvotes photo recommendedBy', { sort: '-upvotes' }, function(err, data) {
 
-		console.log("retrieved " + data.length + " food products from database");
+        if (err) {
+            console.log(err);
+            res.send("Unable to query database for food").status(500);
+        };
 
-		//build and render the template data object, which we will pass into the page
-		var templateData = {
-			food : data,
-			pageTitle : "ITP Snacks! (" + data.length + " Food Products)"
-		}
+        console.log("retrieved " + data.length + " food products from database");
 
-		res.render('index.html', templateData);
+        //build and render the template data object, which we will pass into the page
+        var templateData = {
+            food: data,
+            pageTitle: "ITP Snacks! (" + data.length + " Food Products)"
+        }
 
-	});
+        // res.render('index.html', templateData);
+
+    });
 
 }
 
@@ -55,23 +54,23 @@ exports.index = function(req, res) {
 // api route to return all food as JSON
 exports.allFoodApi = function(req, res) {
 
-	// this is another way of doing a db query, which is identical to the above
-	foodQuery = Food.find({}); // query for all food with no filters
-	foodQuery.sort('-upvotes'); // sort by upvotes
-	
-	// display only these fields from food data
-	foodQuery.select('name slug upvotes photo recommendedBy');
-	
-	foodQuery.exec(function(err, data){
+    // this is another way of doing a db query, which is identical to the above
+    foodQuery = Food.find({}); // query for all food with no filters
+    foodQuery.sort('-upvotes'); // sort by upvotes
 
-		// prepare data for JSON
-		var jsonData = {
-			status : 'OK',
-			food : data
-		}
+    // display only these fields from food data
+    foodQuery.select('name slug upvotes photo recommendedBy');
 
-		res.json(jsonData);
-	});
+    foodQuery.exec(function(err, data) {
+
+        // prepare data for JSON
+        var jsonData = {
+            status: 'OK',
+            food: data
+        }
+
+        res.json(jsonData);
+    });
 
 }
 
@@ -82,37 +81,37 @@ exports.allFoodApi = function(req, res) {
 // get a single food item and display it to user
 exports.oneFood = function(req, res) {
 
-	console.log("detail page requested for food with slug " + req.params.slug);
+    console.log("detail page requested for food with slug " + req.params.slug);
 
-	//get the requested food item by the param on the url /food/:slug
-	var requestedSlug = req.params.slug;
+    //get the requested food item by the param on the url /food/:slug
+    var requestedSlug = req.params.slug;
 
-	// query the database for that food with findOne, searching by its slug
-	// see http://mongoosejs.com/docs/api.html#model_Model.findOne
-	Food.findOne({slug:requestedSlug},function(err, data){
+    // query the database for that food with findOne, searching by its slug
+    // see http://mongoosejs.com/docs/api.html#model_Model.findOne
+    Food.findOne({ slug: requestedSlug }, function(err, data) {
 
-		if (err) {
-			console.log(err);
-			return res.status(500).send("There was an error on the food query");
-		}
+        if (err) {
+            console.log(err);
+            return res.status(500).send("There was an error on the food query");
+        }
 
-		if (data == null) {
-			console.log("couldn't find that food!")
-			return res.status(404).render('404.html');
-		}
+        if (data == null) {
+            console.log("couldn't find that food!")
+            return res.status(404).send('404');
+        }
 
-		console.log("Found the matching food item! --> " + data.name);
+        console.log("Found the matching food item! --> " + data.name);
 
-			//prepare template data to pass into the view
-			var templateData = {
-				food : data,
-				pageTitle : data.name
-			}
+        //prepare template data to pass into the view
+        var templateData = {
+            food: data,
+            pageTitle: data.name
+        }
 
-			// render and return the template
-			res.render('detail.html', templateData);				
-		
-	}); // end of .findOne query
+        // render and return the template
+        res.send('detail.html');
+
+    }); // end of .findOne query
 
 }
 
@@ -125,37 +124,37 @@ exports.oneFood = function(req, res) {
 
 exports.oneFoodApi = function(req, res) {
 
-	console.log("detail page requested for food with slug " + req.params.slug);
+    console.log("detail page requested for food with slug " + req.params.slug);
 
-	//get the requested food item by the param on the url /food/:slug
-	var requestedSlug = req.params.slug;
+    //get the requested food item by the param on the url /food/:slug
+    var requestedSlug = req.params.slug;
 
-	// query the database for that food with findOne, searching by its slug
-	// see http://mongoosejs.com/docs/api.html#model_Model.findOne
-	Food.findOne({slug:requestedSlug},function(err, data){
+    // query the database for that food with findOne, searching by its slug
+    // see http://mongoosejs.com/docs/api.html#model_Model.findOne
+    Food.findOne({ slug: requestedSlug }, function(err, data) {
 
-		if (err) {
-			console.log(err);
-			return res.status(500).send("There was an error on the food query");
-		}
+        if (err) {
+            console.log(err);
+            return res.status(500).send("There was an error on the food query");
+        }
 
-		if (data == null) {
-			console.log("couldn't find that food!")
-			return res.status(404).render('404.html');
-		}
+        if (data == null) {
+            console.log("couldn't find that food!")
+            return res.status(404).send('404.html');
+        }
 
-		console.log("Found the matching food item! --> " + data.name);
+        console.log("Found the matching food item! --> " + data.name);
 
-			//prepare template data to pass into the view
-			var jsonData = {
-				food : data,
-				status : 'OK'
-			}
+        //prepare template data to pass into the view
+        var jsonData = {
+            food: data,
+            status: 'OK'
+        }
 
-			// return the JSON
-			res.json(jsonData);			
-		
-	}); // end of .findOne query
+        // return the JSON
+        res.json(jsonData);
+
+    }); // end of .findOne query
 
 }
 
@@ -164,13 +163,13 @@ exports.oneFoodApi = function(req, res) {
 */
 
 // serves up the add product form
-exports.addFoodForm = function(req, res){
+exports.addFoodForm = function(req, res) {
 
-	var templateData = {
-		pageTitle : 'Suggest a Product!'
-	};
+    var templateData = {
+        pageTitle: 'Suggest a Product!'
+    };
 
-	res.render('create_form.html', templateData);
+    res.send('create_form.html');
 }
 
 /*
@@ -179,46 +178,46 @@ exports.addFoodForm = function(req, res){
 
 // saves a new product to the database from the POST request
 exports.addFoodToDb = function(req, res) {
-	
-	console.log("received form submission");
-	console.log(req.body); // data to save
 
-	// accept form post data
-	// make it into an object we'll save
-	var foodToSave = new Food({
-		name: req.body.name,
-		photo: req.body.photo,
-		recommendedBy: req.body.recommendedBy,
-		slug : req.body.name.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'_') 
-		// the above nifty function takes the name and makes a slug out of it like 
-		// hello-this-is-the-name
-	});
-	
-	// save the food to the database
-	foodToSave.save(function(err,data){
-		if (err) {
-			// if there's an error on saving (perhaps a validation error?)
-			console.log("Error on saving the food");
-			console.log(err); // log out to Terminal all errors
+    console.log("received form submission");
+    console.log(req.body); // data to save
 
-			var templateData = {
-				pageTitle : 'Suggest a Product!',
-				errors : err.errors, 
-				food : req.body
-			};
+    // accept form post data
+    // make it into an object we'll save
+    var foodToSave = new Food({
+        name: req.body.name,
+        photo: req.body.photo,
+        recommendedBy: req.body.recommendedBy,
+        slug: req.body.name.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '_')
+            // the above nifty function takes the name and makes a slug out of it like 
+            // hello-this-is-the-name
+    });
 
-			// re-render the create page, but pass in the templateData
-			// this means the user will see the form with the data they already entered
-			res.render('create_form.html', templateData);
+    // save the food to the database
+    foodToSave.save(function(err, data) {
+        if (err) {
+            // if there's an error on saving (perhaps a validation error?)
+            console.log("Error on saving the food");
+            console.log(err); // log out to Terminal all errors
 
-		} else {
-			console.log("Created a new food!");
-			console.log(data);
-			
-			// redirect to this new food's page
-			res.redirect('/food/'+ data.slug);
-		}
-	});
+            var templateData = {
+                pageTitle: 'Suggest a Product!',
+                errors: err.errors,
+                food: req.body
+            };
+
+            // re-render the create page, but pass in the templateData
+            // this means the user will see the form with the data they already entered
+            res.send('create_form.html');
+
+        } else {
+            console.log("Created a new food!");
+            console.log(data);
+
+            // redirect to this new food's page
+            res.redirect('/food/' + data.slug);
+        }
+    });
 };
 
 /*
@@ -228,38 +227,38 @@ exports.addFoodToDb = function(req, res) {
 //prepares and populates the form to edit a food
 exports.editFoodForm = function(req, res) {
 
-	// Get food by its slug in /food/:slug/edit
-	var requestedSlug = req.params.slug;
+    // Get food by its slug in /food/:slug/edit
+    var requestedSlug = req.params.slug;
 
-	// query the database for that food with findOne, searching by its slug
-	// see http://mongoosejs.com/docs/api.html#model_Model.findOne	
-	Food.findOne({slug:requestedSlug},function(err, data){
+    // query the database for that food with findOne, searching by its slug
+    // see http://mongoosejs.com/docs/api.html#model_Model.findOne	
+    Food.findOne({ slug: requestedSlug }, function(err, data) {
 
-		if (err) {
-			console.error("ERROR");
-			console.error(err);
-			res.send("There was an error querying for "+ requestedSlug).status(500);
-		}
+            if (err) {
+                console.error("ERROR");
+                console.error(err);
+                res.send("There was an error querying for " + requestedSlug).status(500);
+            }
 
-		if (data == null) {
-			console.log("couldn't find that food!")
-			return res.status(404).render('404.html');
-		}
+            if (data == null) {
+                console.log("couldn't find that food!")
+                return res.status(404).send('404.html');
+            }
 
-		if (data != null) {
+            if (data != null) {
 
-			// prepare template data
-			var templateData = {
-				pageTitle: "Edit "+data.name,
-				food: data
-			};
+                // prepare template data
+                var templateData = {
+                    pageTitle: "Edit " + data.name,
+                    food: data
+                };
 
-			// render the edit form, passing in the existing values so the user can see them
-			res.render('edit_form.html',templateData);
+                // render the edit form, passing in the existing values so the user can see them
+                res.send('edit_form.html');
 
-		}
+            }
 
-	}) // end .findOne
+        }) // end .findOne
 
 }
 
@@ -270,43 +269,43 @@ exports.editFoodForm = function(req, res) {
 //takes the data from the POST request and updates the food
 exports.updateFoodToDb = function(req, res) {
 
-	// Get food by its slug in /food/:slug/edit
-	var requestedSlug = req.params.slug;
+    // Get food by its slug in /food/:slug/edit
+    var requestedSlug = req.params.slug;
 
-	// accept form post data
-	// make it into an object we'll save
-	var updatedData = {
-		name: req.body.name,
-		photo: req.body.photo,
-		recommendedBy: req.body.recommendedBy,
-		slug : req.body.name.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'_') 
-		// the above nifty function takes the name and makes a slug out of it like 
-		// hello-this-is-the-name
-	}
+    // accept form post data
+    // make it into an object we'll save
+    var updatedData = {
+        name: req.body.name,
+        photo: req.body.photo,
+        recommendedBy: req.body.recommendedBy,
+        slug: req.body.name.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '_')
+            // the above nifty function takes the name and makes a slug out of it like 
+            // hello-this-is-the-name
+    }
 
 
-	// query and update the requested food
-	// see http://mongoosejs.com/docs/api.html#model_Model.update
-	// $set updates the field if it exists, otherwise it adds it
-	// http://docs.mongodb.org/manual/reference/operator/update/set/
-	Food.update({slug:requestedSlug}, { $set: updatedData}, function(err, data){
+    // query and update the requested food
+    // see http://mongoosejs.com/docs/api.html#model_Model.update
+    // $set updates the field if it exists, otherwise it adds it
+    // http://docs.mongodb.org/manual/reference/operator/update/set/
+    Food.update({ slug: requestedSlug }, { $set: updatedData }, function(err, data) {
 
-		if (err) {
-			console.error("ERROR: While updating");
-			console.error(err);			
-		}
+        if (err) {
+            console.error("ERROR: While updating");
+            console.error(err);
+        }
 
-		if (data != null) {
-			// redirect to that updated food!
-			res.redirect('/food/' + updatedData.slug);
+        if (data != null) {
+            // redirect to that updated food!
+            res.redirect('/food/' + updatedData.slug);
 
-		} else {
+        } else {
 
-			// unable to find that food, return 404
-			console.error("unable to find food: " + data.slug);
-			return res.status(404).render('404.html');
-		}
-	})
+            // unable to find that food, return 404
+            console.error("unable to find food: " + data.slug);
+            return res.status(404).send('404.html');
+        }
+    })
 }
 
 /*
@@ -314,44 +313,44 @@ exports.updateFoodToDb = function(req, res) {
 */
 
 // increments the upvote a given food
-exports.incrementUpvote = function(req,res){
+exports.incrementUpvote = function(req, res) {
 
-	// Get food by its slug in /food/:slug/upvote
-	var requestedSlug = req.params.slug;
+    // Get food by its slug in /food/:slug/upvote
+    var requestedSlug = req.params.slug;
 
-	// query the database for that food with findOne, searching by its slug
-	// see http://mongoosejs.com/docs/api.html#model_Model.findOne	
-	Food.findOne({slug:requestedSlug},function(err, data){
+    // query the database for that food with findOne, searching by its slug
+    // see http://mongoosejs.com/docs/api.html#model_Model.findOne	
+    Food.findOne({ slug: requestedSlug }, function(err, data) {
 
-		if (err) {
-			console.error("ERROR");
-			console.error(err);
-			res.send("There was an error querying for "+ requestedSlug).status(500);
-		}
+            if (err) {
+                console.error("ERROR");
+                console.error(err);
+                res.send("There was an error querying for " + requestedSlug).status(500);
+            }
 
-		if (data == null) {
-			console.log("couldn't find that food!")
-			return res.status(404).render('404.html');
-		}
+            if (data == null) {
+                console.log("couldn't find that food!")
+                return res.status(404).send('404.html');
+            }
 
-		if (data != null) {
+            if (data != null) {
 
-			// now we will update this returned data
-			var newUpvoteCount = data.upvotes + 1; // increment by 1
-			Food.update({slug:requestedSlug},{upvotes:newUpvoteCount},function(err,response){
+                // now we will update this returned data
+                var newUpvoteCount = data.upvotes + 1; // increment by 1
+                Food.update({ slug: requestedSlug }, { upvotes: newUpvoteCount }, function(err, response) {
 
-				if(err){
-					console.log(err);
-					return res.json({status:"ERROR"});
-				}
+                        if (err) {
+                            console.log(err);
+                            return res.json({ status: "ERROR" });
+                        }
 
-				res.json(data); // respond back with JSON
+                        res.json(data); // respond back with JSON
 
-			}) // end update
+                    }) // end update
 
-		}
+            }
 
-	}) // end .findOne
+        }) // end .findOne
 
 }
 
@@ -360,20 +359,20 @@ exports.incrementUpvote = function(req,res){
 */
 
 // deletes a food with a given slug
-exports.deleteFood = function(req,res) {
+exports.deleteFood = function(req, res) {
 
-	// Get food by its slug in /food/:slug/edit
-	var requestedSlug = req.params.slug;
-	
-	// remove the requested food based on its requestedSlug
-	// http://mongoosejs.com/docs/api.html#model_Model-remove
-	Food.remove({slug:requestedSlug}, function(err){
-		if (err){ 
-			console.error(err);
-			res.send("Error when trying to remove food: "+ requestedSlug);
-		}
+    // Get food by its slug in /food/:slug/edit
+    var requestedSlug = req.params.slug;
 
-		res.send("Removed the food! <a href='/'>Back to home</a>.");
-	});
+    // remove the requested food based on its requestedSlug
+    // http://mongoosejs.com/docs/api.html#model_Model-remove
+    Food.remove({ slug: requestedSlug }, function(err) {
+        if (err) {
+            console.error(err);
+            res.send("Error when trying to remove food: " + requestedSlug);
+        }
+
+        res.send("Removed the food! <a href='/'>Back to home</a>.");
+    });
 
 };
