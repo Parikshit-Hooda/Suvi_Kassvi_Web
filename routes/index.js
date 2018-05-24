485454b5363c6d127b8a2af6c3dac2718ef7155e
Parikshit Hooda <phooda804@live.com>
2018-05-24 17:11:10 +0530

    adapt to heroku prod

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@
  * Routes contains the functions (callbacks) associated with request urls.
  */
 
+var express = require('express');
+var router = express.Router();
+
 var Food = require("../models/model.js"); //our db model
 
 
